61641e22cddfa062a9949e6cb32799badd0c0b4d
m7md3bbas <mhmdabass330@gmail.com>
2025-10-27 05:12:11 +0200

    home screen: -implement ui of home

--- lib/features/home/views/home_screen.dart
+++ lib/features/home/views/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:finance_track/core/utils/colors/app_colors.dart';
 import 'package:finance_track/core/utils/helper/greatings/greating_function.dart';
 import 'package:finance_track/core/utils/helper/ui/customcurvecliper.dart';
 import 'package:finance_track/features/auth/logic/login/login_cubit.dart';
+import 'package:finance_track/features/home/views/widgets/cardexpensesandincome.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,95 +14,154 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HomeScreen();
+    return const HomeScreen();
   }
 }
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final userName =
+        context.read<LoginCubit>().user?.userMetadata?['name'] ?? '';
+
     return Scaffold(
       extendBodyBehindAppBar: true,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
+      backgroundColor: Colors.grey[100],
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Stack(
+              clipBehavior: Clip.none,
               children: [
                 ClipPath(
                   clipper: BottomCurveClipper(),
                   child: Container(
                     width: double.infinity,
-                    height: 250.h,
+                    height: 230.h,
                     decoration: BoxDecoration(
                       color: Colors.white.modify(
                         colorCode: AppColors.mainAppColor,
                       ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: RichText(
-                              text: TextSpan(
-                                children: [
-                                  TextSpan(
-                                    text: "${getGreetingMessage()}, ",
-                                    style: GoogleFonts.inter(
-                                      fontSize: 16.sp,
-                                      fontWeight: FontWeight.w300,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  TextSpan(
-                                    text:
-                                        "\n${context.read<LoginCubit>().user?.userMetadata?.entries.map((e) => e.key == "name" ? e.value : "").join(" ")}",
-                                    style: GoogleFonts.inter(
-                                      fontSize: 20.sp,
-                                      fontWeight: FontWeight.w300,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-
-                          Spacer(),
-                          Card(
-                            color: Colors.white.modify(
-                              colorCode: AppColors.mainCardColor,
-                            ),
-                            child: IconButton(
-                              icon: const Icon(
-                                Icons.notifications,
-                                color: Colors.white,
-                              ),
-                              onPressed: () {},
-                            ),
-                          ),
-                        ],
-                      ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16.w,
+                      vertical: 32.h,
                     ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: "${getGreetingMessage()}, ",
+                                  style: GoogleFonts.inter(
+                                    fontSize: 16.sp,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: "\n$userName",
+                                  style: GoogleFonts.inter(
+                                    fontSize: 20.sp,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Card(
+                          elevation: 2,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.r),
+                          ),
+                          color: Colors.white.modify(
+                            colorCode: AppColors.mainCardColor,
+                          ),
+                          child: IconButton(
+                            icon: const Icon(
+                              Icons.notifications,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                Positioned(
+                  bottom: -70.h,
+                  left: 20.w,
+                  right: 20.w,
+                  child: const BalanceCard(
+                    totalBalance: 2548.00,
+                    income: 1840.00,
+                    expenses: 284.00,
                   ),
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          SliverToBoxAdapter(child: SizedBox(height: 80.h)),
+
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Row(
+                children: [
+                  Text(
+                    "Recent Transactions",
+                    style: GoogleFonts.inter(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  const Spacer(),
+                  Text(
+                    "See all",
+                    style: GoogleFonts.inter(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          SliverPadding(
+            padding: EdgeInsets.all(8.w),
+            sliver: SliverList.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) => Card(
+                elevation: 1,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.r),
+                ),
+                child: ListTile(
+                  title: Text("Transaction #$index"),
+                  subtitle: const Text("Transaction duration"),
+                  trailing: const Text(
+                    "\$100",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
